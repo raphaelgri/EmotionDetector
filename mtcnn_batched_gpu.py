5ac888914ada5162f98c7ec673cef129e4c41c8b
Raphael G Duarte <raphael.griffo-duarte@stud.th-deg.de>
2022-06-24 19:21:39 +0200

    timestamp and better frame selection

--- mtcnn_batched_gpu.py
+++ mtcnn_batched_gpu.py
@@ -3,6 +3,7 @@ import cv2
 from PIL import Image
 import numpy as np
 from matplotlib import pyplot as plt
+import math
 #from tqdm.notebook import tqdm
 
 #initialize the model, selecting GPU as device and keeping all faces
@@ -11,6 +12,8 @@ mtcnn = MTCNN(select_largest=False, device='cuda', keep_all=True,post_process=Fa
 #Load the video
 v_cap = cv2.VideoCapture('Data/videoplayback.mp4')
 v_len = int(v_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+fps = int(v_cap.get(cv2.CAP_PROP_FPS))
+timestamps = [] #list for the timestamps
 
 # Loop through video
 batch_size = 64
@@ -21,12 +24,13 @@ landmarks = []
 view_frames = []
 view_boxes = []
 view_landmarks = []
+sf = 1 # get one frame per second
 
 for i in range(v_len):
     # Load frame
     success, frame = v_cap.read()
     #select every n frames
-    if i % 30 == 0:
+    if i % math.floor(fps)*sf == 0: #skip to get every sf seconds
         success, frame = v_cap.retrieve()
     else:
         continue
@@ -38,15 +42,18 @@ for i in range(v_len):
     frame = Image.fromarray(frame)
     frame = frame.resize([int(f * 0.25) for f in frame.size])
     frames.append(frame)
+    timestamps.append(math.floor(v_cap.get(cv2.CAP_PROP_POS_MSEC))*1000) #append timestamp of the frame
+    print(math.floor(v_cap.get(cv2.CAP_PROP_POS_MSEC))*1000)
     
     # When batch is full, detect faces and reset batch list
     if len(frames) >= batch_size:
         #print("Executed MTCNN batch num" + str(batch_num))
         #define save path 
-        save_paths = [f'Data/T1/image_{i}_{batch_num}.jpg' for i in range(len(frames))]
+        save_paths = [f'Data/T2/image_t{timestamps[i]}.jpg' for i in range(len(frames))]
         #save all faces to files
         faces = mtcnn(frames, save_path=save_paths)
         im_count = 0
         #increase batch number
         batch_num += 1
-        frames = []
+        frames = [] #clear frame list
+        timestamps = [] #clear timestamplist
